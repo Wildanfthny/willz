f769ea8e09da59c24777a41983cbc00f8d743d10
Wildanfthny <60375220+Wildanfthny@users.noreply.github.com>
2020-02-17 18:40:46 +0700

    Delete db_con.php

--- db_con.php
+++
@@ -1,7 +0,0 @@
-<?php
-$database = mysqli_connect("localhost","id12502062_root","12345","id12502062_db_login");
- 
-if (mysqli_connect_errno()){
-	echo "Gagal melakukan koneksi ke MySQL: " . mysqli_connect_error();
-}
-?>
